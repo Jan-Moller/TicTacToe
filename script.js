d2bb338313eb6648d2682be1d00b8e7f4210dc5a
Jan-Moller <ja-moeller@web.de>
2023-04-28 20:50:33 +0200

    update

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ function fillShape(id) {
 
     if (currentShape == 'cross') {
         currentShape = 'circle';
+        document.getElementById('player-2').classList.remove('player-inactive');
+        document.getElementById('player-1').classList.add('player-inactive');
     }
     else {
         currentShape = 'cross';
+        document.getElementById('player-1').classList.remove('player-inactive');
+        document.getElementById('player-2').classList.add('player-inactive');
     }
 
     fields[id] = currentShape;
