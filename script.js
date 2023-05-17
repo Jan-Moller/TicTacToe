deb5e8aaa3f217abe43facd5d2d6564015c359ea
Jan-Moller <ja-moeller@web.de>
2023-05-17 11:23:51 +0200

    final

--- script.js
+++ script.js
@@ -21,6 +21,22 @@ function fillShape(id) {
     }
 }
 
+function restart() {
+    gameOver = false;
+    fields = [];
+    document.getElementById('game-over').classList.add('d-none');
+    document.getElementById('restart-btn').classList.add('d-none');
+
+    for (let i = 1; i < 8; i++) {
+        document.getElementById('line-' + i).classList.add('d-none');
+    }
+
+    for (let i = 0; i < 9; i++) {
+        document.getElementById('circle_' + i).classList.add('d-none');
+        document.getElementById('cross_' + i).classList.add('d-none');
+    }
+}
+
 function draw() {
     for (let i = 0; i < fields.length; i++) {
         if (fields[i] == 'circle') {
@@ -79,8 +95,10 @@ function checkForWin() {
     if (winner) {
         console.log('Gewonnen', winner);
         gameOver = true;
-        setTimeout(function(){
-            document.getElementById('game-over').classList.remove('d-none')
+        setTimeout(function () {
+            document.getElementById('game-over').classList.remove('d-none');
+            document.getElementById('restart-btn').classList.remove('d-none');
+
         }, 1000);
     }
 }
